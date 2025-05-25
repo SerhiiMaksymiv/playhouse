296ccadf5d5523be102c1b67a55d9a009463d321
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2025-05-25 22:16:40 +0300

    upd lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,9 @@
-import { playAudit } from './audit.js';
+import { report } from './report.js';
 export * from './types.js';
-export { playAudit };
+export { report };
+declare const _default: {
+    audit: (auditConfig: import("./types.js").LightherConfig) => Promise<import("./types.js").ScoreResult>;
+};
+export default _default;
 export declare const version = "1.0.0";
 export declare const libraryName = "playhouse";
